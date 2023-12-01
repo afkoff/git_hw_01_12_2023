f460eed004293d7fc64aa3582815fc47524c92b2
Aleksandr Afk <aleksandrafk17@gmail.com>
2023-12-01 10:39:41 +0300

    search for max commit

---
+++ search_for_max.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+	int n;
+	int max = INT_MIN;
+	cin >> n;
+	int *arr = new int[n];
+	for(int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+		if(arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	cout << max << endl;
+	return 0;
+}
